bf8d9fcbee893f04aa7cd2ae0c0ccf6fb61460f3
w452261940 <wende47@126.com>
2023-02-04 18:13:44 -0500

    Mainmenu Update

--- Assets/GameLogic/UIManager.cs
+++ Assets/GameLogic/UIManager.cs
@@ -2,10 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 
 public class UIManager : MonoBehaviour
 {
+    [SerializeField] Slider volumeSlider;
+
+
     public void StartGame()
     {
         SceneManager.LoadScene(1);
@@ -30,5 +34,10 @@ public class UIManager : MonoBehaviour
     public void LoadMainMenu()
     {
         SceneManager.LoadScene(0);
+    }
+
+    public void ChangeVolume()
+    {
+        AudioListener.volume = volumeSlider.value;
     }
 }
